0c3c72938c1914c9c82799b9dd58f93a284be948
Jay Sorg <jay.sorg@gmail.com>
2018-08-15 22:02:36 -0700

    work

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ CFLAGS=-O2 -Wall -fvisibility=hidden -fPIC
 
 LDFLAGS=
 
-LIBS=fox/finder_gui.a `fox-config-1.6 --libs`
+LIBS=fox/finder_gui.a `fox-config-1.6 --libs` -lstdc++
 
 all:
 	$(MAKE) -C fox
--- fox/finder_gui.cpp
+++ fox/finder_gui.cpp
@@ -7,12 +7,81 @@
 
 #include "finder_gui.h"
 
+class MsgObject;
+
 struct app_data
 {
     FXApp* app;
     FXMainWindow* mw;
-    FXButton* but;
+    FXButton* but1;
+    FXButton* but2;
+    FXTabBar* tb;
+    FXTabItem* ti1;
+    FXTabItem* ti2;
+    FXTabItem* ti3;
+    MsgObject* mo;
 };
+
+class MsgObject : public FXObject
+{
+    FXDECLARE(MsgObject)
+public:
+    MsgObject();
+    long onDefault(FXObject* obj, FXSelector sel, void* ptr);
+    long onPress(FXObject* obj, FXSelector sel, void* ptr);
+    long onTabChange(FXObject* obj, FXSelector sel, void* ptr);
+    struct app_data* ap;
+    enum _ids
+    {
+        ID_BUTTON = 0,
+        ID_TABBAR,
+        ID_LAST
+    } ids;
+};
+
+/*****************************************************************************/
+MsgObject::MsgObject()
+{
+}
+
+/*****************************************************************************/
+long
+MsgObject::onDefault(FXObject* obj, FXSelector sel, void* ptr)
+{
+    return FXObject::onDefault(obj, sel, ptr);
+}
+
+/*****************************************************************************/
+long
+MsgObject::onPress(FXObject* obj, FXSelector sel, void* ptr)
+{
+    printf("onPress obj %p sel %d ptr %p\n", obj, sel, ptr);
+    if (obj == ap->but1)
+    {
+        printf("but1\n");
+    }
+    else
+    {
+        printf("but2\n");
+    }
+    return 0; 
+}
+
+/*****************************************************************************/
+long
+MsgObject::onTabChange(FXObject* obj, FXSelector sel, void* ptr)
+{
+    printf("onTabChange obj %p sel %d ptr %p\n", obj, sel, ptr);
+    return 0;
+}
+
+FXDEFMAP(MsgObject) MsgObjectMap[] =
+{
+    FXMAPFUNC(SEL_COMMAND, MsgObject::ID_BUTTON, MsgObject::onPress),
+    FXMAPFUNC(SEL_COMMAND, MsgObject::ID_TABBAR, MsgObject::onTabChange)
+};
+
+FXIMPLEMENT(MsgObject, FXObject, MsgObjectMap, ARRAYNUMBER(MsgObjectMap))
 
 /*****************************************************************************/
 void*
@@ -21,10 +90,17 @@ gui_create(int argc, char** argv)
     struct app_data* ap;
 
     ap = (struct app_data*)calloc(sizeof(struct app_data), 1);
-    ap->app = new FXApp("Hello", "FoxTest");
+    ap->app = new FXApp("Find", "Find");
     ap->app->init(argc, argv);
-    ap->mw = new FXMainWindow(ap->app, "Hello", NULL, NULL, DECOR_ALL);
-    ap->but = new FXButton(ap->mw, "&Hello, World!", NULL, ap->app, FXApp::ID_QUIT);
+    ap->mo = new MsgObject();
+    ap->mo->ap = ap;
+    ap->mw = new FXMainWindow(ap->app, "Find", NULL, NULL, DECOR_ALL, 0, 0, 640, 480);
+    ap->tb = new FXTabBar(ap->mw, ap->mo, MsgObject::ID_TABBAR, TABBOOK_NORMAL);
+    ap->ti1 = new FXTabItem(ap->tb, "Name & Location", 0, TAB_TOP_NORMAL);
+    ap->ti2 = new FXTabItem(ap->tb, "Date Modified", 0, TAB_TOP_NORMAL);
+    ap->ti3 = new FXTabItem(ap->tb, "Advanced", 0, TAB_TOP_NORMAL);
+    ap->but1 = new FXButton(ap->mw, "but1", NULL, ap->mo, MsgObject::ID_BUTTON);
+    ap->but2 = new FXButton(ap->mw, "but2", NULL, ap->mo, MsgObject::ID_BUTTON);
     ap->app->create();
     ap->mw->show(PLACEMENT_SCREEN);
     return ap;
@@ -49,6 +125,7 @@ gui_delete(void* han)
 
     ap = (struct app_data*)han;
     delete ap->app;
+    delete ap->mo;
     free(ap);
     return 0;
 }
