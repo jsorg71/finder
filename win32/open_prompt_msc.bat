ee5a26188afdf12c6efb55a77a4c35a5c53b02db
Jay Sorg <jay.sorg@gmail.com>
2019-08-15 21:44:57 -0700

    win32: use visual stdio compatible env vars

--- win32/open_prompt_msc.bat
+++ win32/open_prompt_msc.bat
@@ -1,33 +1,18 @@
 @echo off
 
+rem normally you should use your visual stdio Tools Command Prompt
+rem or you can use this and build with winddk
+
 rem winddk 32 bit
-set DDKPATH=C:\WinDDK\7600.16385.1
-set PATH=%DDKPATH%\bin\x86\x86;%DDKPATH%\bin\x86;c:\windows;c:\windows\system32
-set FINDER_LDFLAGS=/libpath:%DDKPATH%\lib\Crt\i386 /libpath:%DDKPATH%\lib\win7\i386
-set FINDER_CFLAGS=/I%DDKPATH%\inc\crt /I%DDKPATH%\inc\api
-set FINDER_LIBS=user32.lib advapi32.lib shell32.lib ole32.lib gdi32.lib comctl32.lib
+rem set DDKPATH=C:\WinDDK\7600.16385.1
+rem set PATH=%DDKPATH%\bin\x86\x86;%DDKPATH%\bin\x86;c:\windows;c:\windows\system32
+rem set INCLUDE=/I%DDKPATH%\inc\crt;%DDKPATH%\inc\api
+rem set LIB=%DDKPATH%\lib\Crt\i386;%DDKPATH%\lib\win7\i386
 
 rem winddk 64 bit
-rem set DDKPATH=C:\WinDDK\7600.16385.1
-rem set PATH=%DDKPATH%\bin\x86\amd64;%DDKPATH%\bin\x86;c:\windows;c:\windows\system32
-rem set FINDER_LDFLAGS=/libpath:%DDKPATH%\lib\Crt\amd64 /libpath:%DDKPATH%\lib\win7\amd64
-rem set FINDER_CFLAGS=/I%DDKPATH%\inc\crt /I%DDKPATH%\inc\api
-rem set FINDER_LIBS=user32.lib advapi32.lib shell32.lib ole32.lib gdi32.lib comctl32.lib
-
-rem vc2010 32 bit
-rem set VCPATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0
-rem set SDKPATH=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A
-rem set PATH=%VCPATH%\VC\bin;%VCPATH%\Common7\IDE;c:\windows;c:\windows\system32
-rem set FINDER_LDFLAGS=/libpath:"%VCPATH%\VC\lib" /libpath:"%SDKPATH%\Lib"
-rem set FINDER_CFLAGS=/I"%VCPATH%\VC\include" /I"%SDKPATH%\Include"
-rem set FINDER_LIBS=user32.lib advapi32.lib shell32.lib ole32.lib gdi32.lib comctl32.lib
-
-rem vc2010 64 bit
-rem set VCPATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0
-rem set SDKPATH=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A
-rem set PATH=%VCPATH%\VC\bin\x86_amd64;%VCPATH%\VC\bin;%VCPATH%\Common7\IDE;c:\windows;c:\windows\system32
-rem set FINDER_LDFLAGS=/libpath:"%VCPATH%\VC\lib\amd64" /libpath:"%SDKPATH%\Lib\x64"
-rem set FINDER_CFLAGS=/I"%VCPATH%\VC\include" /I"%SDKPATH%\Include"
-rem set FINDER_LIBS=user32.lib advapi32.lib shell32.lib ole32.lib gdi32.lib comctl32.lib
+set DDKPATH=C:\WinDDK\7600.16385.1
+set PATH=%DDKPATH%\bin\x86\amd64;%DDKPATH%\bin\x86;c:\windows;c:\windows\system32
+set INCLUDE=%DDKPATH%\inc\crt;%DDKPATH%\inc\api
+set LIB=%DDKPATH%\lib\Crt\amd64;%DDKPATH%\lib\win7\amd64
 
 cmd
