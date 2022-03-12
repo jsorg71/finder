bfa24948cf1cac001dbeddf9f6be10baa6e41620
Jay Sorg <jay.sorg@gmail.com>
2022-03-11 21:33:39 -0800

    gtk2: add some gui functions

--- gtk2/finder_gui.c
+++ gtk2/finder_gui.c
@@ -5,10 +5,47 @@
 
 #include <gtk/gtk.h>
 
+#include "finder.h"
+#include "finder_event.h"
+#include "finder_gui.h"
+#include "finder_gui_about.h"
+#include "finder_mainwindow.h"
+
+/*****************************************************************************/
 int
 main(int argc, char** argv)
 {
     gtk_init(&argc, &argv);
     gtk_main();
+    return 0;
+}
+
+/*****************************************************************************/
+int
+gui_set_event(struct finder_info* fi)
+{
+    return 0;
+}
+
+/*****************************************************************************/
+int
+gui_find_done(struct finder_info* fi)
+{
+    return 0;
+}
+
+/*****************************************************************************/
+int
+gui_add_one(struct finder_info* fi, const char* filename,
+            const char* in_subfolder, FINDER_I64 size,
+            const char* modified)
+{
+    return 0;
+}
+
+/*****************************************************************************/
+int
+gui_writeln(struct finder_info* fi, const char* msg)
+{
     return 0;
 }
