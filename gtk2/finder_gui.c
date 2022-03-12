637d09c6aba42a155f37fd9ec39536c766137610
Jay Sorg <jay.sorg@gmail.com>
2022-03-11 16:40:29 -0800

    start adding gtk2 files

---
+++ gtk2/finder_gui.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <gtk/gtk.h>
+
+int
+main(int argc, char** argv)
+{
+    gtk_init(&argc, &argv);
+    gtk_main();
+    return 0;
+}
