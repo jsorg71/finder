67c541d6b78326d6c8e7a4e290769dfd90b1be03
Jay Sorg <jay.sorg@gmail.com>
2023-04-09 15:05:43 -0700

    win32: fix for winddk build 32 bit

--- win32/open_prompt_msc.bat
+++ win32/open_prompt_msc.bat
@@ -6,7 +6,7 @@ rem or you can use this and build with winddk
 rem winddk 32 bit
 rem set DDKPATH=C:\WinDDK\7600.16385.1
 rem set PATH=%DDKPATH%\bin\x86\x86;%DDKPATH%\bin\x86;c:\windows;c:\windows\system32
-rem set INCLUDE=/I%DDKPATH%\inc\crt;%DDKPATH%\inc\api
+rem set INCLUDE=%DDKPATH%\inc\crt;%DDKPATH%\inc\api
 rem set LIB=%DDKPATH%\lib\Crt\i386;%DDKPATH%\lib\win7\i386
 
 rem winddk 64 bit
